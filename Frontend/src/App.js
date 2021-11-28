5314cf7ad2b9787fc5f45335bded77ef65701dff
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2021-11-28 17:17:47 +0500

    routes added

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,10 +1,22 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import BookingCar from "./pages/BookingCar";
 
 function App() {
   return (
     <div className="App">
-      <h1>CRP</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<Home />}></Route>
+          <Route path="/login" exact element={<Login />}></Route>
+          <Route path="/register" exact element={<Register />}></Route>
+          <Route path="/bookingcar" exact element={<BookingCar />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- Frontend/src/pages/Home.js
+++ Frontend/src/pages/Home.js
@@ -3,7 +3,13 @@ import React from "react";
 function Home() {
   return (
     <div>
-      <h1>Home Page</h1>
+      <h3>hellow</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi eligendi
+        nihil dignissimos saepe, fuga a perspiciatis cum, mollitia labore quos
+        voluptatem hic tempore impedit. Aspernatur doloribus voluptas tempora
+        saepe qui?
+      </p>
     </div>
   );
 }
