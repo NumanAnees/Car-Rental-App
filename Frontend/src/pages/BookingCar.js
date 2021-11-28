4be6e828ffcf42587a6ad460c015127709c5a8dd
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2021-11-28 16:12:12 +0500

    pages added

---
+++ Frontend/src/pages/BookingCar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function BookingCar() {
+  return (
+    <div>
+      <h1>Booking car page</h1>
+    </div>
+  );
+}
+
+export default BookingCar;
---
+++ Frontend/src/pages/Register.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Register() {
+  return (
+    <div>
+      <h1>Register page</h1>
+    </div>
+  );
+}
+
+export default Register;
