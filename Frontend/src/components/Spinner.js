d7ec63b96cd702660ab36d5ab0187c7e12c49330
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2021-11-30 20:26:06 +0500

    loading added

---
+++ Frontend/src/components/Spinner.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Spin } from "antd";
+function Spinner() {
+  return (
+    <div className="Spinner">
+      <Spin size="large" />
+    </div>
+  );
+}
+
+export default Spinner;
--- Frontend/src/pages/Home.js
+++ Frontend/src/pages/Home.js
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import DefaultLayout from "../components/DefaultLayout";
 import { getAllCars } from "../redux/actions/carsAction";
 import { Button, Row, Col } from "antd";
+import Spinner from "../components/Spinner";
 
 function Home() {
   const { cars } = useSelector((state) => state.carsReducer);
@@ -13,6 +14,7 @@ function Home() {
   }, []);
   return (
     <DefaultLayout>
+      {loading == true && <Spinner />}
       <Row justify="center" gutter={16} className="mt-5">
         {cars.map((car) => {
           return (
