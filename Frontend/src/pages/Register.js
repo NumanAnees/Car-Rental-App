6e97e42c8f473cfe756a24e1f6b35f2a3dd579f6
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2021-12-04 14:09:25 +0500

    userAction implemented

--- Frontend/src/pages/Register.js
+++ Frontend/src/pages/Register.js
@@ -1,13 +1,24 @@
 import React from "react";
 import { Row, Col, Form, Input } from "antd";
 import { Link } from "react-router-dom";
-
+import { useDispatch } from "react-redux";
+import { userRegister } from "../redux/actions/userActions";
 function Register() {
+  const dispatch = useDispatch();
+
+  function onFinish(values) {
+    dispatch(userRegister(values));
+    console.log(values);
+  }
   return (
     <div className="login">
       <Row gutter={8}>
         <Col lg={8} className="text-left p-5">
-          <Form layout="vertical" className="login-form p-5">
+          <Form
+            layout="vertical"
+            className="login-form p-5"
+            onFinish={onFinish}
+          >
             <h1 className="login-heading">Register</h1>
             <hr />
             <Form.Item
---
+++ Frontend/src/redux/actions/userActions.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { message } from "antd";
+
+export const userLogin = (reqObj) => async (dispatch) => {
+  dispatch({ type: "LOADING", payload: true });
+
+  try {
+    const response = await axios.post("api/users/login", reqObj);
+    localStorage.setItem("user", JSON.stringify(response.data));
+    message.success("Login success");
+    dispatch({ type: "LOADING", payload: false });
+    setTimeout(() => {
+      window.location.href = "/";
+    }, 500);
+  } catch (error) {
+    console.log(error);
+    message.error("Something went wrong");
+    dispatch({ type: "LOADING", payload: false });
+  }
+};
+
+export const userRegister = (reqObj) => async (dispatch) => {
+  dispatch({ type: "LOADING", payload: true });
+
+  try {
+    const response = await axios.post("api/users/register", reqObj);
+    message.success("Registration successfull");
+    setTimeout(() => {
+      window.location.href = "/login";
+    }, 500);
+
+    dispatch({ type: "LOADING", payload: false });
+  } catch (error) {
+    console.log(error);
+    message.error("Something went wrong");
+    dispatch({ type: "LOADING", payload: false });
+  }
+};
