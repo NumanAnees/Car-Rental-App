c99b08abbbf0b66f8d03756263589bd983e9be9c
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2022-01-03 18:58:02 +0500

    Why us

--- Frontend/src/components/Services.js
+++ Frontend/src/components/Services.js
@@ -1,4 +1,10 @@
 import React from "react";
+import Fast_Booking from "../images/icons/fast.png";
+import Collection from "../images/icons/collection.png";
+import Insurance from "../images/icons/insurance.png";
+import Map from "../images/icons/map.png";
+import Pay from "../images/icons/pay.png";
+import Support from "../images/icons/support.png";
 
 const Services = () => {
   return (
@@ -8,18 +14,19 @@ const Services = () => {
           <div class="row">
             <div class="col-md-12">
               <div class="site-heading text-center">
-                <h2>
-                  What we <span>Offer</span>
+                <h2 className="heading-service">
+                  Why Choose<span> US</span>?
                 </h2>
-                <h4>Lorem Ipsum is simply dummy text</h4>
               </div>
             </div>
           </div>
           <div class="row our-offer-items less-carousel">
             <div class="col-md-4 col-sm-6 equal-height">
               <div class="item">
-                <i class="fas fa-pen-fancy"></i>
-                <h4>Project creation</h4>
+                <i class="fas fa-pen-fancy">
+                  <img src={Fast_Booking} alt="" className="icon-image" />
+                </i>
+                <h4>Fast & Easy Booking</h4>
                 <p>
                   Lorem Ipsum is simply dummy text of the printing and
                   typesetting industry.
@@ -29,8 +36,10 @@ const Services = () => {
 
             <div class="col-md-4 col-sm-6 equal-height">
               <div class="item">
-                <i class="fas fa-dharmachakra"></i>
-                <h4>Software Development</h4>
+                <i class="fas fa-dharmachakra">
+                  <img src={Map} alt="" className="icon-image" />
+                </i>
+                <h4>Any Pickup Location</h4>
                 <p>
                   Lorem Ipsum is simply dummy text of the printing and
                   typesetting industry.
@@ -40,8 +49,10 @@ const Services = () => {
 
             <div class="col-md-4 col-sm-6 equal-height">
               <div class="item">
-                <i class="fas fa-tasks"></i>
-                <h4>Porject Management</h4>
+                <i class="fas fa-tasks">
+                  <img src={Pay} alt="" className="icon-image" />
+                </i>
+                <h4>No Booking Charges</h4>
                 <p>
                   Lorem Ipsum is simply dummy text of the printing and
                   typesetting industry.
@@ -51,8 +62,10 @@ const Services = () => {
 
             <div class="col-md-4 col-sm-6 equal-height">
               <div class="item">
-                <i class="fas fa-tachometer-alt"></i>
-                <h4>Porject Impliment</h4>
+                <i class="fas fa-tachometer-alt">
+                  <img src={Collection} alt="" className="icon-image" />
+                </i>
+                <h4>Large Collection</h4>
                 <p>
                   Lorem Ipsum is simply dummy text of the printing and
                   typesetting industry.
@@ -62,8 +75,10 @@ const Services = () => {
 
             <div class="col-md-4 col-sm-6 equal-height">
               <div class="item">
-                <i class="fas fa-recycle"></i>
-                <h4>Software Update</h4>
+                <i class="fas fa-recycle">
+                  <img src={Insurance} alt="" className="icon-image" />
+                </i>
+                <h4>Fully Insured</h4>
                 <p>
                   Lorem Ipsum is simply dummy text of the printing and
                   typesetting industry.
@@ -73,7 +88,9 @@ const Services = () => {
 
             <div class="col-md-4 col-sm-6 equal-height">
               <div class="item">
-                <i class="fas fa-headset"></i>
+                <i class="fas fa-headset">
+                  <img src={Support} alt="" className="icon-image" />
+                </i>
                 <h4>24/7 Support</h4>
                 <p>
                   Lorem Ipsum is simply dummy text of the printing and
