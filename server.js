56762a71572a5257d5b0ed418b0a1b37c73efed2
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2022-01-05 03:18:24 +0500

    dynamic port

--- server.js
+++ server.js
@@ -4,18 +4,11 @@ const app = express();
 const dbConnection = require("./Db/db");
 app.use(express.json());
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 app.get("/", (req, res) => {
   res.send("hello");
 });
 
-if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
-  });
-}
 app.use("/api/cars/", require("./Routes/carsRoutes"));
 //refresh error
 app.use("/booking/api/cars/", require("./Routes/carsRoutes"));
@@ -23,7 +16,13 @@ app.use("/editcar/api/cars/", require("./Routes/carsRoutes"));
 app.use("/api/users/", require("./Routes/usersRoutes"));
 app.use("/booking/api/bookings/", require("./Routes/bookingsRoute"));
 app.use("/api/bookings/", require("./Routes/bookingsRoute"));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 app.listen(port, () => {
   console.log(`Server is running at port: ${port} `);
 });
