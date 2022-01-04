6474576e68a8377ca370cf2ed7c4c516df837ff7
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2022-01-05 01:25:03 +0500

    final

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ app.get("/", (req, res) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static("Frontend/build"));
+  app.use("/", express.static("client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "Frontend/build/index.html"));
+    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
   });
 }
 app.use("/api/cars/", require("./Routes/carsRoutes"));
