32ab9edfa4497695305497e36b0d501f12ac2bbb
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2023-01-09 03:51:41 +0500

    cors issue fix

--- server.js
+++ server.js
@@ -2,27 +2,31 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const dbConnection = require("./Db/db");
+var cors = require("cors");
+app.use(cors());
+
 app.use(express.json());
 
 const port = process.env.PORT || 8000;
 
-app.use("/api/cars/", require("./Routes/carsRoutes"));
 app.get("/hello", (req, res) => {
   res.send("Hello!");
 });
+
+app.use("/api/cars/", require("./Routes/carsRoutes"));
 //refresh error
 app.use("/booking/api/cars/", require("./Routes/carsRoutes"));
 app.use("/editcar/api/cars/", require("./Routes/carsRoutes"));
 app.use("/api/users/", require("./Routes/usersRoutes"));
 app.use("/booking/api/bookings/", require("./Routes/bookingsRoute"));
 app.use("/api/bookings/", require("./Routes/bookingsRoute"));
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
 app.listen(port, () => {
   console.log(`Server is running at port: ${port} `);
 });
